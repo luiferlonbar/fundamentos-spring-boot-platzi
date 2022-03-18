294fd4dcf9934020f408b2ba385caf6f2fee8b35
luiferlonbar <luiferlonbar@gmail.com>
2022-03-18 18:15:01 -0500

    first commit

---
+++ src/main/java/com/fundamentosplatzi/springnboot/fuendamentos/FuendamentosApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springnboot.fuendamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuendamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuendamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosplatzi/springnboot/fuendamentos/FuendamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springnboot.fuendamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuendamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
